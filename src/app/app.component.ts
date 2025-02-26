787869c757407e271c1e12f1e0665f93192ff222
velazquezxana597@gmail.com <velazquezxana597@gmail.com>
2025-02-26 07:58:02 -0700

    hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductoComponent } from './listado-productos/producto/producto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tienda-online';
+  title = 'Act6';
 }
---
+++ src/app/listado-productos/producto/producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  imports: [],
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.css'
+})
+export class ProductoComponent {
+Descripcion = 'Nuevo Producto';
+Precio = 100;
+}
